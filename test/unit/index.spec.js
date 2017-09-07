88381500e586fffb34393c3f621db683eeee4383
Marcos Augusto <mjaojr@gmail.com>
2017-09-07 15:27:40 -0300

    test(): add unit test file

---
+++ test/unit/index.spec.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('Test application', () => {
+    it('should be true', () => {
+        expect(1 == 1).to.be.equal(true);
+    });
+});
